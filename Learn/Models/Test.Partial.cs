dff492196f3384ca08fb3928f3c2c48799c95771
Unidyna-824-PC\Unidyna-824 <wayneli@unidyna.com>
2018-10-16 10:31:23 +0800

    更新partial檔

--- Learn/Models/Test.Partial.cs
+++ Learn/Models/Test.Partial.cs
@@ -18,6 +18,9 @@ namespace Learn.Models
         public string TestName { get; set; }
         public Nullable<bool> TestIsDeleted { get; set; }
         public Nullable<int> TestCategoryId { get; set; }
+        
+        [StringLength(128, ErrorMessage="欄位長度不得大於 128 個字元")]
+        public string TestCode { get; set; }
     
         public virtual TestCategory TestCategory { get; set; }
     }
